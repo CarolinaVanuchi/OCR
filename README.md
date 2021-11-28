3fbd5c7a8b0ad508542f651aad001c286a530f91
CarolinaVanuchi <carolinavanuchi21@gmail.com>
2021-11-28 15:33:33 -0800

    start using tesseract  for ocr

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# OCR
+# Project assigment using Tesseract and OpenCV  
+
+- [Project assigment using Tesseract and OpenCV](#project-assigment-using-tesseract-and-opencv)
+- [OCR](#ocr)
+  - [References](#references)
+- [Pattern matching](#pattern-matching)
+
+# OCR 
+## References 
+    - https://nanonets.com/blog/ocr-with-tesseract/
+
+# Pattern matching
--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 import cv2
 import pytesseract
+from src import treatment
+
+image = cv2.imread('src/image/exemple.png')
+
+
+window_name = 'image'
+cv2.imshow(window_name, image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+gray = treatment.Treatment.put_gray(image)
+thresh = treatment.Treatment.thresholding(gray)
+opening = treatment.Treatment.opening(gray)
+canny = treatment.Treatment.canny(gray)
+
+
+window_name2 = 'canny'
+cv2.imshow(window_name2, canny)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+custom_config = r'--oem 3 --psm 6'
+text = pytesseract.image_to_string(canny, config=custom_config)
+print(text)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
